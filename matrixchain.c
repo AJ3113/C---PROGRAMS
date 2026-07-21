2c04e7d3395103c100a3284e046570f8ee1ad843
AJ <aj5662528@gmail.com>
2026-07-21 16:08:09 +0530

    Matrix chain multplication
    Finding minimum number of multiplications needed for multiplying multiple matrices

---
+++ matrixchain.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAX 100
+#define INF 999999
+
+int matrixChainOrder(int p[], int n)
+{
+    int m[MAX][MAX];
+
+    // Cost is 0 when multiplying one matrix
+    for (int i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L = chain length
+    for (int L = 2; L < n; L++)
+    {
+        for (int i = 1; i < n - L + 1; i++)
+        {
+            int j = i + L - 1;
+            m[i][j] = INF;
+
+            for (int k = i; k < j; k++)
+            {
+                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+
+    return m[1][n - 1];
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of matrices: ");
+    scanf("%d", &n);
+
+    int p[n + 1];
+
+    printf("Enter the dimensions array (%d values):\n", n + 1);
+    for (int i = 0; i <= n; i++)
+        scanf("%d", &p[i]);
+
+    printf("Minimum number of multiplications = %d\n",
+           matrixChainOrder(p, n + 1));
+
+    return 0;
+}

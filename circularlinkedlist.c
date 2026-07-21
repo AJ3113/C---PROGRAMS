c1c14bbd0aea42389fcbafdac84eed9399bf11f3
AJ <aj5662528@gmail.com>
2026-07-21 15:45:31 +0530

    Circular linked list implementation

---
+++ circularlinkedlist.c
@@ -0,0 +1,147 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* head=NULL;
+void insertatfront(int val)
+{
+    struct node*newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL)
+    {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newnode->data=val;
+    if(head==NULL){
+    head=newnode;
+     newnode->next=head;
+    }
+    else{
+        struct node* temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        newnode->next=head;
+        head=newnode;
+        temp->next=head;
+    }
+    printf("Element inserted at front successfully\n");
+}
+void insertatend(int val)
+{
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL)
+    {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newnode->data=val;
+    if(head==NULL){
+        newnode->next=head;
+        head=newnode;
+    }
+    else{
+        struct node* current=head;
+        while(current->next!=head)
+        {
+            current=current->next;
+        }
+        current->next=newnode;
+        newnode->next=head;
+        
+    }
+    printf("Element inserted at end successfully\n");
+}
+void deleteatfront()
+{
+    if(head==NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    if(head->next==head)
+    {
+        head=NULL;
+    }
+    else{
+        struct node* temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        head=head->next;
+        temp->next=head;
+    }
+     printf("Element deleted at front successfully\n");
+}
+void deleteatend()
+{
+    if(head==NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+     if(head->next==head)
+    {
+        free(head);
+        head=NULL;
+        printf("Element deleted at end successfully\n");
+    }
+    else{
+        struct node* current=head;
+        while(current->next->next!=head)
+        {
+            current=current->next;
+        }
+        free(current->next);
+        current->next=head;
+        printf("Element deleted at end successfully\n");
+    }
+}
+void  display()
+{
+    if(head==NULL)
+    {
+        printf("List is empty\n");
+    }
+    else{
+        struct node* current=head;
+        printf("List elements:\n");
+        do{
+            printf("%d ",current->data);
+            current=current->next;
+        }
+        while(current!=head);
+        printf("\n");
+    }
+}
+int main() {
+    int choice;
+    while(1) {
+        printf("\nSingly Linked List Operations:\n");
+        printf("1. Insert Front  2. Insert End  3. Delete Front  4. Delete End  5. Display  6. Exit\n");
+        printf("Enter choice: "); scanf("%d", &choice);
+        switch(choice) {
+            case 1: {
+                int val;
+                printf("Enter value to insert at front: ");
+                scanf("%d", &val);
+                insertatfront(val);
+                break;
+            }
+            case 2: {
+                int val;
+                printf("Enter value to insert at end: ");
+                scanf("%d", &val);
+                insertatend(val);
+                break;
+            }
+            case 3: deleteatfront(); break;
+            case 4: deleteatend(); break;
+            case 5: display(); break; case 6: exit(0);
+        }
+    }
+}
